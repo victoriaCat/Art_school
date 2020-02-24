676078f0410ff2bffb407c8d830c594d29c01e86
victoriaCat <katysheva.victoria@gmail.com>
2020-02-24 14:32:59 +0300

    changed api

--- art_school/src/libs/api.js
+++ art_school/src/libs/api.js
@@ -1,2 +1,2 @@
-export const IMGS_API = 'https://pushkareva.art/gallery/api/';
-export const FS_IMG_URL = 'https://pushkareva.art/fs/gallery/';
+export const IMGS_API = 'https://pushkareva.art:1443/gallery/api/';
+export const FS_IMG_URL = 'https://pushkareva.art:1443/fs/gallery/';
