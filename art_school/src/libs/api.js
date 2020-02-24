8cfa25129a5bc786b0c177f59481ee66217bbf54
victoriaCat <katysheva.victoria@gmail.com>
2020-02-24 15:00:09 +0300

    delete all

--- art_school/src/libs/api.js
+++
@@ -1,2 +0,0 @@
-export const IMGS_API = 'https://pushkareva.art:1443/gallery/api/';
-export const FS_IMG_URL = 'https://pushkareva.art:1443/fs/gallery/';
--- art_school/src/store/combinedReducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import mainPageImages from '~/modules/WelcomeBlockMainPage/reducer';
-import aboutMePageImages from '~/modules/AboutMe/reducer';
-
-export default combineReducers({
-    mainPageImages,
-    aboutMePageImages
-});
