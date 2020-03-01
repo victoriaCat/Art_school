4a3d50cf5cc51f13010df45e3885a95c95a74ce6
victoriaCat <katysheva.victoria@gmail.com>
2020-03-01 17:00:38 +0300

    feedback form post request

--- art_school/src/modules/Contacts/components/FeedbackForm/FeedbackForm.jsx
+++ art_school/src/modules/Contacts/components/FeedbackForm/FeedbackForm.jsx
@@ -1,19 +1,60 @@
 import './FeedbackForm.scss';
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-const FeedbackForm = () => {
-    return (
-        <form action="#" method="post">
-            <fieldset>
-                <p><input type="text" id="name" placeholder="Ваше имя" required/></p>
-                <p><input type="email" id="email" placeholder="Ваш электронный адрес" required/></p>
-                <p><input type="text" id="phone" placeholder="Ваш номер телефона" required/></p>
-                <p><textarea id="message" cols="100" rows="10" placeholder="Текст Вашего сообщения">
+class FeedbackForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            phone: '',
+            message: ''
+        };
+        this.handleChange=this.handleChange.bind(this);
+        this.handleSubmit=this.handleSubmit.bind(this);
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        const name = this.state.name;
+        const email = this.state.email;
+        const phone = this.state.phone;
+        const message = this.state.message;
+        const data = {
+            name,
+            email,
+            phone,
+            message
+        };
+        axios.post("https://jsonplaceholder.typicode.com/users", data)
+            .then(response =>{
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+    handleChange(e){
+        e.preventDefault();
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                    <p><input type="text" name="name" placeholder="Ваше имя" value={this.state.name} onChange={this.handleChange} required/></p>
+                    <p><input type="email" name="email" placeholder="Ваш электронный адрес" value={this.state.email} onChange={this.handleChange}
+                              required/></p>
+                    <p><input type="text" name="phone" placeholder="Ваш номер телефона" value={this.state.phone} onChange={this.handleChange}
+                              required/></p>
+                    <p><textarea name="message" cols="100" rows="10" placeholder="Текст Вашего сообщения"
+                                 value={this.state.message} onChange={this.handleChange}>
                 </textarea></p>
-            </fieldset>
-            <p><input type="submit" value="ОТПРАВИТЬ"/></p>
-        </form>
-    );
-};
+                <p><input type="submit" value="ОТПРАВИТЬ"/></p>
+            </form>
+        );
+    }
+}
 
 export default FeedbackForm;
