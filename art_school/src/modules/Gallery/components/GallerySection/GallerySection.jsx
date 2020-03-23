8e3138456678a337b039ea5c10114b46538f2877
victoriaCat <katysheva.victoria@gmail.com>
2020-03-23 16:43:32 +0300

    gallery changes

--- art_school/src/modules/Gallery/components/GallerySection/GallerySection.jsx
+++ art_school/src/modules/Gallery/components/GallerySection/GallerySection.jsx
@@ -6,10 +6,12 @@ const GallerySection = (props) => {
     return (
         <div className='gallery-section'>
             <h3>{props.header}</h3>
-            {props.images.map(album => album.map(image => (<div key={image.id} className='gallery-item'>
-                <a href={`${FS_IMG_URL}${image.id}/${image.name}?`} target="_blank"><img
-                    src={`${FS_IMG_URL}${image.id}/${image.name}?s=gt`} alt={image.name}/></a>
-            </div>)))}
+            <div className="gallery-items-wrap">
+                {props.images.map(album => album.map(image => (<div key={image.id} className='gallery-item'>
+                    <a href={`${FS_IMG_URL}${image.id}/${image.name}?`} target="_blank"><img
+                        src={`${FS_IMG_URL}${image.id}/${image.name}?s=gt`} alt={image.name}/></a>
+                </div>)))}
+            </div>
         </div>
     );
 };
