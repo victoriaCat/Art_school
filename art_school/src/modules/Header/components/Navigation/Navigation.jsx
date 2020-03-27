c253c23e7c672c5868d8c92a543d235b175cf102
victoriaCat <katysheva.victoria@gmail.com>
2020-03-27 20:35:48 +0300

    desktop style changes

--- art_school/src/modules/Header/components/Navigation/Navigation.jsx
+++ art_school/src/modules/Header/components/Navigation/Navigation.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {HashLink} from 'react-router-hash-link';
 import arrowImg from '~/assets/images/arrow.png';
-import stickImg from '~/assets/images/flower-stick.png';
 
 const Navigation = () => {
     return (
@@ -14,7 +13,6 @@ const Navigation = () => {
                         <li className="nav-list-item"><HashLink to="/#options">ЗАНЯТИЯ</HashLink></li>
                         <li className="nav-list-item">РАСПИСАНИЕ <img className="schedule-arrow" src={arrowImg} alt=""/>
                             <div className="schedule-links">
-                                <img src={stickImg} alt=""/>
                                 <ul>
                                     <li><Link to="/workshops/adults">взрослые</Link></li>
                                     <li><Link to="/workshops/kids">дети</Link></li>
