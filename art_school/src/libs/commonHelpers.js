836136018d42021370343a774a2d31f2cec36193
victoriaCat <katysheva.victoria@gmail.com>
2020-03-30 13:41:17 +0300

    fixing decoding string

--- art_school/src/libs/commonHelpers.js
+++ art_school/src/libs/commonHelpers.js
@@ -1,5 +1,7 @@
+import React from "react";
+
 export function decodeString(str) {
     if (typeof (window) === 'object') {
         return Buffer.from(str, 'base64').toString('utf8');
-    } else return str;
+    } else return ' ';
 }
