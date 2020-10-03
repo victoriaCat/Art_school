f5d0c10d88636e5dbfbfa42d6ae0f84d68d36772
Victoria Katysheva <katysheva.victoria@gmail.com>
2020-10-03 19:58:03 +0300

    routing with analytics changes

--- art_school/src/modules/Workshop/helpers.js
+++ art_school/src/modules/Workshop/helpers.js
@@ -5,17 +5,18 @@ export function findWorkshop(workshopsArray, workshopId) {
 }
 
 export function determineAlbum(path) {
-    let album;
+    let album, name;
     const linkArr = path.split('/');
+    name = linkArr[linkArr.length - 1].split(/([&?])/) ? linkArr[linkArr.length - 1].split(/([&?])/)[0] : linkArr[linkArr.length - 1];
     if (linkArr.some((item) => {
         return item === 'adults';
     })) {
-        album = 'workshop_' + linkArr[linkArr.length - 1] + '_adults';
+        album = 'workshop_' + name + '_adults';
     }
     if (linkArr.some((item) => {
         return item === 'kids';
     })) {
-        album = 'workshop_' + linkArr[linkArr.length - 1] + '_kids';
+        album = 'workshop_' + name + '_kids';
     }
     return album;
 }
